4f92dbf6f529e7859b42a381809479ac869ea0d1
Freddiecoder99 <rickyfredo77@gmail.com>
2024-11-14 19:30:09 +0000

    updated

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -4,54 +4,78 @@ from flask import abort, jsonify, request
 from models.user import User
 
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
-def view_all_users():
+def view_all_users() -> str:
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
-def view_one_user(user_id):
+def view_one_user(user_id: str = None) -> str:
+    if user_id is None:
+        abort(404)
     if user_id == 'me':
         if request.current_user is None:
             abort(404)
-        user = request.current_user
-    else:
-        user = User.get(user_id)
-        if not user:
-            abort(404)
+        user_id = request.current_user.id
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
     return jsonify(user.to_json())
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
-def delete_user(user_id):
+def delete_user(user_id: str = None) -> str:
+    if user_id is None:
+        abort(404)
     user = User.get(user_id)
-    if not user:
+    if user is None:
         abort(404)
     user.remove()
     return jsonify({}), 200
 
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
-def create_user():
-    rj = request.get_json()
-    if not rj or rj.get("email") == "" or rj.get("password") == "":
-        return jsonify({'error': "Wrong format"}), 400
-    user = User()
-    user.email = rj.get("email")
-    user.password = rj.get("password")
-    user.first_name = rj.get("first_name")
-    user.last_name = rj.get("last_name")
-    user.save()
-    return jsonify(user.to_json()), 201
+def create_user() -> str:
+    rj = None
+    error_msg = None
+    try:
+        rj = request.get_json()
+    except Exception as e:
+        rj = None
+    if rj is None:
+        error_msg = "Wrong format"
+    if error_msg is None and rj.get("email", "") == "":
+        error_msg = "email missing"
+    if error_msg is None and rj.get("password", "") == "":
+        error_msg = "password missing"
+    if error_msg is None:
+        try:
+            user = User()
+            user.email = rj.get("email")
+            user.password = rj.get("password")
+            user.first_name = rj.get("first_name")
+            user.last_name = rj.get("last_name")
+            user.save()
+            return jsonify(user.to_json()), 201
+        except Exception as e:
+            error_msg = "Can't create User: {}".format(e)
+    return jsonify({'error': error_msg}), 400
 
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
-def update_user(user_id):
-    user = User.get(user_id)
-    if not user:
+def update_user(user_id: str = None) -> str:
+    if user_id is None:
         abort(404)
-    rj = request.get_json()
-    if not rj:
+    user = User.get(user_id)
+    if user is None:
+        abort(404)
+    rj = None
+    try:
+        rj = request.get_json()
+    except Exception as e:
+        rj = None
+    if rj is None:
         return jsonify({'error': "Wrong format"}), 400
-    if 'first_name' in rj:
-        user.first_name = rj['first_name']
-    if 'last_name' in rj:
-        user.last_name = rj['last_name']
+    if rj.get('first_name') is not None:
+        user.first_name = rj.get('first_name')
+    if rj.get('last_name') is not None:
+        user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
+
