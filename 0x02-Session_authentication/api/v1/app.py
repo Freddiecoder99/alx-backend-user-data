a21de2cd750e7083f8c49f8e71671e8996d0f257
Freddiecoder99 <rickyfredo77@gmail.com>
2024-11-17 22:03:16 +0000

    updated

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -5,47 +5,83 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import CORS
-import os
+from flask_cors import (CORS, cross_origin)
+
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
 auth = None
-auth_type = os.environ.get('AUTH_TYPE')
+auth_type = getenv("AUTH_TYPE", "auth")
 
-if auth_type == 'basic_auth':
+
+if auth_type == "basic_auth":
     from api.v1.auth.basic_auth import BasicAuth
     auth = BasicAuth()
-elif auth_type == 'session_auth':
+elif auth_type == "session_auth":
     from api.v1.auth.session_auth import SessionAuth
     auth = SessionAuth()
+elif auth_type == "session_exp_auth":
+    from api.v1.auth.session_exp_auth import SessionExpAuth
+    auth = SessionExpAuth()
+elif auth_type == "session_db_auth":
+    from api.v1.auth.session_db_auth import SessionDBAuth
+    auth = SessionDBAuth()
+else:
+    from api.v1.auth.auth import Auth
+    auth = Auth()
+
 
 @app.before_request
-def request_filter():
-    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
-    if auth and auth.require_auth(request.path, excluded_paths):
-        if not auth.authorization_header(request) and not auth.session_cookie(request):
+def auth_filter():
+    """
+    Filter each request
+    """
+    if auth is None:
+        return
+
+    excluded_path = ['/api/v1/status/',
+                     '/api/v1/unauthorized/',
+                     '/api/v1/forbidden/',
+                     '/api/v1/auth_session/login/']
+
+    if auth.require_auth(request.path, excluded_path):
+        if (auth.authorization_header(request) is None and
+                auth.session_cookie(request) is None):
             abort(401)
-        current_user = auth.current_user(request)
-        if current_user is None:
+
+        user = auth.current_user(request)
+        if user is None:
             abort(403)
-        request.current_user = current_user
 
-@app.errorhandler(401)
-def unauthorized(error):
-    return jsonify({"error": "Unauthorized"}), 401
+    request.current_user = auth.current_user(request)
 
-@app.errorhandler(403)
-def forbidden(error):
-    return jsonify({"error": "Forbidden"}), 403
 
 @app.errorhandler(404)
-def not_found(error):
+def not_found(error) -> str:
+    """
+    Not found handler
+    """
     return jsonify({"error": "Not found"}), 404
+
+
+@app.errorhandler(401)
+def unauthorized(error) -> str:
+    """
+    401 Unauthorized handler
+    """
+    return jsonify({"error": "Unauthorized"}), 401
+
+
+@app.errorhandler(403)
+def forbidden(error) -> str:
+    """
+    403 Forbidden handler
+    """
+    return jsonify({"error": "Forbidden"}), 403
+
 
 if __name__ == "__main__":
     host = getenv("API_HOST", "0.0.0.0")
     port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+    app.run(host=host, port=port, debug=True)
--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,28 +1,57 @@
 #!/usr/bin/env python3
+""" Module of Users views
+"""
 from api.v1.views import app_views
 from flask import abort, jsonify, request
 from models.user import User
 
+
 @app_views.route('/users', methods=['GET'], strict_slashes=False)
 def view_all_users() -> str:
+    """ GET /api/v1/users
+    Return:
+      - list of all User objects JSON represented
+    """
     all_users = [user.to_json() for user in User.all()]
     return jsonify(all_users)
 
+
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def view_one_user(user_id: str = None) -> str:
+    """ GET /api/v1/users/:id
+    Path parameter:
+      - User ID
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+    """
     if user_id is None:
         abort(404)
-    if user_id == 'me':
+
+    if user_id == "me":
         if request.current_user is None:
             abort(404)
-        user_id = request.current_user.id
+        else:
+            return jsonify(request.current_user.to_json())
+
     user = User.get(user_id)
     if user is None:
         abort(404)
+    if request.current_user is None:
+        abort(404)
+
     return jsonify(user.to_json())
+
 
 @app_views.route('/users/<user_id>', methods=['DELETE'], strict_slashes=False)
 def delete_user(user_id: str = None) -> str:
+    """ DELETE /api/v1/users/:id
+    Path parameter:
+      - User ID
+    Return:
+      - empty JSON is the User has been correctly deleted
+      - 404 if the User ID doesn't exist
+    """
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -31,8 +60,19 @@ def delete_user(user_id: str = None) -> str:
     user.remove()
     return jsonify({}), 200
 
+
 @app_views.route('/users', methods=['POST'], strict_slashes=False)
 def create_user() -> str:
+    """ POST /api/v1/users/
+    JSON body:
+      - email
+      - password
+      - last_name (optional)
+      - first_name (optional)
+    Return:
+      - User object JSON represented
+      - 400 if can't create the new User
+    """
     rj = None
     error_msg = None
     try:
@@ -58,8 +98,20 @@ def create_user() -> str:
             error_msg = "Can't create User: {}".format(e)
     return jsonify({'error': error_msg}), 400
 
+
 @app_views.route('/users/<user_id>', methods=['PUT'], strict_slashes=False)
 def update_user(user_id: str = None) -> str:
+    """ PUT /api/v1/users/:id
+    Path parameter:
+      - User ID
+    JSON body:
+      - last_name (optional)
+      - first_name (optional)
+    Return:
+      - User object JSON represented
+      - 404 if the User ID doesn't exist
+      - 400 if can't update the User
+    """
     if user_id is None:
         abort(404)
     user = User.get(user_id)
@@ -78,4 +130,3 @@ def update_user(user_id: str = None) -> str:
         user.last_name = rj.get('last_name')
     user.save()
     return jsonify(user.to_json()), 200
-
